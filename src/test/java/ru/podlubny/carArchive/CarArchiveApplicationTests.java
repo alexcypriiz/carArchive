9098bd4d05664b29a7f16c705d40d3d0b1a62a0f
alexcypriiz <alexey.podlubny@yandex.ru>
2023-06-06 23:33:36 +0400

    init commit

---
+++ src/test/java/ru/podlubny/carArchive/CarArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.podlubny.carArchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarArchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
